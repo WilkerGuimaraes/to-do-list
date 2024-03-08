504ce86210c56936420d4ea31eb9edb63002a68d
Wilker Guimaraes <wilkerguimaraes.07@gmail.com>
2024-03-07 22:27:23 -0300

    feat: add content to TaskDetails

--- src/components/TasksDetails.jsx
+++ src/components/TasksDetails.jsx
@@ -1,7 +1,28 @@
+import { useParams } from "react-router-dom";
+
+import Button from "./Button";
+
 import { TaskDetailsContainer } from "./TasksDetails.style";
 
 const TaskDetails = () => {
-  return <TaskDetailsContainer></TaskDetailsContainer>;
+  const params = useParams();
+
+  return (
+    <TaskDetailsContainer>
+      <div className="back-button-container">
+        <Button>Back</Button>
+      </div>
+
+      <div className="task-details-content">
+        <h2>{params.taskTitle}</h2>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro minus
+          dolorum ipsum sapiente excepturi laudantium eligendi alias.
+          Architecto, eos eaque.
+        </p>
+      </div>
+    </TaskDetailsContainer>
+  );
 };
 
 export default TaskDetails;
--- src/components/TasksDetails.style.js
+++ src/components/TasksDetails.style.js
@@ -1,3 +1,30 @@
 import styled from "styled-components";
 
-export const TaskDetailsContainer = styled.div``;
+export const TaskDetailsContainer = styled.div`
+  .back-button-container {
+    width: 40%;
+    margin-top: 10px;
+  }
+
+  .task-details-content {
+    display: flex;
+    flex-direction: column;
+    padding: 15px 20px;
+    margin: 15px 0;
+    background-color: #444;
+    border-radius: 5px;
+
+    h2 {
+      color: chartreuse;
+      margin-bottom: 5px;
+
+      &::first-letter {
+        text-transform: capitalize;
+      }
+    }
+
+    p {
+      color: #eee;
+    }
+  }
+`;
